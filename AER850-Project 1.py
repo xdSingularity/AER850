130ce06b0c441f623d76da84336577c4d0ec4796
xdSingularity <ilan.boublil@torontomu.ca>
2023-10-14 18:35:48 -0400

    RFR variable name
    RFR variables for anything related to the random forest regressor

--- AER850-Project 1.py
+++ AER850-Project 1.py
@@ -17,8 +17,6 @@ df = pd.read_csv('Project 1 Data.csv')
 x = np.array(df['X'])
 y = np.array(df['Y'])
 z = np.array(df['Z'])
-step_data = df['Step'].copy()
-df = df.drop('Step', axis=1)
 
 plot1 = plt.figure()
 ax1 = plot1.add_subplot(111, projection='3d')
@@ -35,7 +33,7 @@ plt.show()
 ## part 3 ###################################################################################################################
 
 # 1. Compute the Correlation
-correlation_matrix = df.corr()
+correlation_matrix = df.iloc[:, :-1].corr()
 
 # 2. Visualize the Correlation
 plt.figure(figsize=(10, 8))
@@ -44,15 +42,15 @@ plt.title('Correlation Heatmap')
 plt.show()
 
 ## part 4 ###################################################################################################################
-
 from sklearn.model_selection import StratifiedShuffleSplit
 # Assuming you're stratifying based on the 'Z' column
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(df, step_data):
+for train_index, test_index in split.split(df, df['Step']):
     strat_train_set = df.loc[train_index].reset_index(drop=True)
     strat_test_set = df.loc[test_index].reset_index(drop=True)
 
-
+train_X = strat_train_set[['X', 'Y', 'Z']]
+train_Y = strat_train_set['Step']
 
 
 ## part 5 ###################################################################################################################
