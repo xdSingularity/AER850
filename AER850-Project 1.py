bf0285c8f3bfe9b6fb60c8d26a03c9dd70c062bb
xdSingularity <ilan.boublil@torontomu.ca>
2023-10-14 21:22:43 -0400

    change from logistic regression to decision tree
    logistic regression was giving too many issues so it was changed to decision tree model

--- AER850-Project 1.py
+++ AER850-Project 1.py
@@ -42,11 +42,11 @@ plt.title('Correlation Heatmap')
 plt.show()
 
 ## part 4 ###################################################################################################################
-X = df['X','Y','Z']
+X = df[['X','Y','Z']]
 Y = df['Step']
 
 from sklearn.model_selection import StratifiedShuffleSplit
-# Assuming you're stratifying based on the 'Z' column
+# Assuming you're stratifying based on the 'Step' column
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(X, Y):
     train_X, test_X = X.iloc[train_index], X.iloc[test_index]
@@ -57,25 +57,25 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 RFC = RandomForestClassifier(n_estimators=100, random_state=42)
 RFC.fit(train_X, train_Y)
-RFC_predictions = RFC.predict(train_X)
-RFC_train_accuracy = accuracy_score(RFC_predictions, train_Y)
-print("Random Forest Classifier training accuracy is: ", round(RFC_train_accuracy, 5))
+RFC_predictions = RFC.predict(test_X)
+RFC_test_accuracy = accuracy_score(RFC_predictions, test_Y)
+print("Random Forest Classifier test accuracy (before best hyper parameters) is: ", round(RFC_test_accuracy, 5))
 
 #model 2 LOGISTIC REGRESSION
 from sklearn.linear_model import LogisticRegression
 LR = LogisticRegression(max_iter=1000) # Increasing max_iter for convergence
 LR.fit(train_X, train_Y)
-LR_predictions = LR.predict(train_X)
-LR_train_accuracy = accuracy_score(LR_predictions, train_Y)
-print("Logistic Regression training accuracy is: ", round(LR_train_accuracy, 5))
+LR_predictions = LR.predict(test_X)
+LR_test_accuracy = accuracy_score(LR_predictions, test_Y)
+print("Logistic Regression test accuracy (before best hyper parameters) is: ", round(LR_test_accuracy, 5))
 
 #model 3 SVM (SVC)
 from sklearn.svm import SVC
-SVM_clf = SVC(kernel="linear")  # Using a linear kernel for simplicity. You can try other kernels like 'rbf'.
+SVM_clf = SVC()  # Using a linear kernel for simplicity. You can try other kernels like 'rbf'.
 SVM_clf.fit(train_X, train_Y)
-SVM_predictions = SVM_clf.predict(train_X)
-SVM_train_accuracy = accuracy_score(SVM_predictions, train_Y)
-print("Support Vector Machine Classifier training accuracy is: ", round(SVM_train_accuracy, 5))
+SVM_predictions = SVM_clf.predict(test_X)
+SVM_test_accuracy = accuracy_score(SVM_predictions, test_Y)
+print("Support Vector Machine Classifier test accuracy (before best hyper parameters) is: ", round(SVM_test_accuracy, 5))
 
 # GRIDSEARCH CV
 from sklearn.model_selection import GridSearchCV
@@ -93,7 +93,7 @@ models = [
     },
     {
         'name': 'LogisticRegression',
-        'model': LogisticRegression(random_state=42, max_iter=10000),
+        'model': LogisticRegression(random_state=42, max_iter=100000),
         'param_grid': [{'penalty': ['l1'],'C': [0.001, 0.01, 0.1, 1, 10, 100],'solver': ['liblinear', 'saga']},
             {'penalty': ['l2'],'C': [0.001, 0.01, 0.1, 1, 10, 100],'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']},
             {'penalty': [None],'solver': ['newton-cg', 'lbfgs', 'sag', 'saga']}
@@ -125,6 +125,41 @@ SVM_params = best_params_dict['SVC']
 
 ## part 5 ###################################################################################################################
 
+# Training RFC with best hyperparameters
+RFC_best = RandomForestClassifier(**RFC_params)
+RFC_best.fit(train_X, train_Y)
+RFC_best_predictions = RFC_best.predict(test_X)
+
+# Training LR with best hyperparameters
+LR_best = LogisticRegression(**LR_params)
+LR_best.fit(train_X, train_Y)
+LR_best_predictions = LR_best.predict(test_X)
+
+# Training SVM with best hyperparameters
+SVM_best = SVC(**SVM_params)
+SVM_best.fit(train_X, train_Y)
+SVM_best_predictions = SVM_best.predict(test_X)
+
+from sklearn.metrics import f1_score, precision_score, accuracy_score
+
+# Metrics Calculation
+def compute_metrics(predictions, true_values):
+    accuracy = accuracy_score(predictions, true_values)
+    precision = precision_score(predictions, true_values, average='weighted')  # Considering multi-class problem
+    f1 = f1_score(predictions, true_values, average='weighted')  # Considering multi-class problem
+    return accuracy, precision, f1
+
+# RFC metrics
+RFC_accuracy, RFC_precision, RFC_f1 = compute_metrics(RFC_best_predictions, test_Y)
+print(f"\nRandom Forest Classifier - Accuracy: {round(RFC_accuracy, 5)}, Precision: {round(RFC_precision, 5)}, F1 Score: {round(RFC_f1, 5)}")
+
+# LR metrics
+LR_accuracy, LR_precision, LR_f1 = compute_metrics(LR_best_predictions, test_Y)
+print(f"\nLogistic Regression - Accuracy: {round(LR_accuracy, 5)}, Precision: {round(LR_precision, 5)}, F1 Score: {round(LR_f1, 5)}")
+
+# SVM metrics
+SVM_accuracy, SVM_precision, SVM_f1 = compute_metrics(SVM_best_predictions, test_Y)
+print(f"\nSupport Vector Machine Classifier - Accuracy: {round(SVM_accuracy, 5)}, Precision: {round(SVM_precision, 5)}, F1 Score: {round(SVM_f1, 5)}")
 
 
 
